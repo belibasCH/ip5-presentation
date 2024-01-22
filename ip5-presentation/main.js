00c3b4ee969f68a66a1740c29b9a0a629f23c776
Francesco Lucia <francesco.lucia@students.fhnw.ch>
2024-01-22 22:24:47 +0100

    added content in analysen

--- ip5-presentation/main.js
+++ ip5-presentation/main.js
@@ -3,11 +3,22 @@ import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm.js';
 
 
-
 let deck = new Reveal({
-   plugins: [ Markdown, RevealHighlight ],
+   plugins: [ Markdown, RevealHighlight, RevealChart ],
 })
 deck.initialize({
-
+        chart: {
+            defaults: {
+                color: 'black', // color of labels
+                scale: {
+                    beginAtZero: true,
+                    ticks: { stepSize: 1 },
+                    grid: { color: "lightgray" } , // color of grid lines
+                },
+            },
+            line: { borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ], "borderDash": [ [5,10], [0,0] ] },
+            bar: { backgroundColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ]},
+            pie: { backgroundColor: [ ["rgba(30,30,30,.8)" , "rgba(220,20,20,.8)", "rgba(20,220,20,.8)", "rgba(220,220,20,.8)", "rgba(20,20,220,.8)"] ]},
+        }
 }
 );
